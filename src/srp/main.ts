7a1e568e2178e89bf03a261f849fa813089d9682
feitosadavi <dfeitosa.dev@protonmail.com>
2021-01-08 21:02:57 -0300

    Adicionado OCP

--- src/srp/main.ts
+++ src/srp/main.ts
@@ -1,12 +1,20 @@
-// Classes coesas são classes que usam seus próprios atributos
-
+/**
+ * Open/closed principle
+ * Entidades devem estar abertas para extensão e fechadas para modificação
+ *
+ * Agora o código está fechado para modificação, mas aberto para extensão,
+ * já que eu não preciso mais modificar o shopping cart para adicionar desconto.
+ * Tendo que extender o discount para adicionar novos discontos
+ */
 import { Messaging } from './services/messaging';
-import { Order } from './entities/order';
+import { Order } from './classes/order';
 import { Persistency } from './services/persistency';
-import { Product } from './entities/product';
-import { ShoppingCart } from './entities/shopping-cart';
+import { Product } from './classes/product';
+import { ShoppingCart } from './classes/shopping-cart';
+import { PerDayDiscout } from './classes/discount';
 
-const shoppingCart = new ShoppingCart();
+const perDayDiscout = new PerDayDiscout();
+const shoppingCart = new ShoppingCart(perDayDiscout);
 const messaging = new Messaging();
 const persistency = new Persistency();
 const order = new Order(shoppingCart, messaging, persistency);
@@ -16,7 +24,7 @@ shoppingCart.addItem(new Product('Tênis', 149.9123));
 shoppingCart.addItem(new Product('Bermuda', 60.0));
 
 console.log(shoppingCart.items);
-console.log(shoppingCart.total());
+console.log(shoppingCart.totalWithDiscount());
 console.log(order.orderStatus);
 order.checkout();
 console.log(order.orderStatus);
